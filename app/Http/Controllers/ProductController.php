8f7edb22be3e1377b52d4a29d06f016659310e0e
Ngọc Thạch <ngocthach.2004bp@gmail.com>
2025-02-25 22:01:45 +0700

    add file SQL

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,17 +6,14 @@ use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    // Lấy danh sách sản phẩm
     public function index()
     {
         $products = Product::all();
         return response()->json($products);
     }
 
-    // Thêm sản phẩm mới
     public function store(Request $request)
     {
-        // Validate dữ liệu đầu vào
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'price' => 'required|numeric|min:0',
@@ -26,7 +23,6 @@ class ProductController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
-        // Tạo sản phẩm mới
         $product = Product::create([
             'name' => $request->name,
             'price' => $request->price,
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Định nghĩa route cho sản phẩm
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
